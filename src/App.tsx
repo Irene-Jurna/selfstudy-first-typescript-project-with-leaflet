d619f77855ff107d47d98d6c33190fa85974bb1e
Irene Jurna <irene.jurna@gmail.com>
2023-05-17 21:07:11 +0200

    Markers worden getoond op de kaart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import mushrooms, {Mushroom} from './front-end api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    const [mushroomData, setMushroomData] = useState<Mushroom[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await mushrooms();
+                setMushroomData(response);
+                console.log(response);
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Code Challenge Fungus Friends</h1>
+            <MapContainer center={[52.081, 5.236]} zoom={23} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {mushroomData.map((mushrooms) => (
+                    <Marker
+                        key={mushrooms.name}
+                        position={mushrooms.latlng}>
+                        <Popup>
+                            <h4>{mushrooms.name}</h4>
+                            {/*<p>Spots: {mushrooms.spots}</p>*/}
+                            {/*<p>Color: {mushrooms.color}</p>*/}
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
